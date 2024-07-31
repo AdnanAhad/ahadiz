faaac6292cdd427d72e650aba119a0600b7b448b
adnanahad <adnanahad.in@gmail.com>
2024-07-31 23:39:28 +0530

    Added login and Registration

--- ahadizbackend/index.js
+++ ahadizbackend/index.js
@@ -43,6 +43,27 @@ app.get("/users", async (req, res) => {
   }
 });
 
+// Login
+
+app.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    // Find a user with the given userName and password
+    const user = await User.findOne({ userName, password });
+    if (user) {
+      // User found, login successful
+      res.status(200).json({ message: "Login successful", user });
+    } else {
+      // User not found or incorrect password
+      res.status(401).json({ message: "Invalid username or password" });
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Server error during login" });
+  }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`API server running on http://localhost:${port}`);
