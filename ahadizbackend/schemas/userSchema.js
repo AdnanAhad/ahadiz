dac68a2502a9ea0c4c373fbab9d9d133fa267476
adnanahad <adnanahad.in@gmail.com>
2024-07-31 11:09:58 +0530

    first commit

---
+++ ahadizbackend/schemas/userSchema.js
@@ -0,0 +1,44 @@
+// schemas/userSchema.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = userSchema;
